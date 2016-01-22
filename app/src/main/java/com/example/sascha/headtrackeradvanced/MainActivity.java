130909dbe3ea4c81d28ee8e06508d0e03c7598d6
astralaster <chalvadori@gmx.de>
2016-01-22 17:25:45 +0100

    accelerometer, gravity und gyroscope funktionieren

--- app/src/main/java/com/example/sascha/headtrackeradvanced/MainActivity.java
+++ app/src/main/java/com/example/sascha/headtrackeradvanced/MainActivity.java
@@ -20,9 +20,14 @@ import android.content.Intent;
 import android.net.Uri;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 
 import android.content.Context;
+
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
 
 
 
@@ -30,7 +35,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     int value;
     private SensorManager mSensorManager;
-    private Sensor mSensor;
+    private Sensor accelerometer;
+    private Sensor gravity;
+    private Sensor gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +56,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(this, mSensor, 1);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        gyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        mSensorManager.registerListener(this, accelerometer, 1);
+        mSensorManager.registerListener(this, gravity, 1);
+        mSensorManager.registerListener(this, gyroscope, 1);
 
     }
 
@@ -80,9 +92,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        ((TextView) findViewById(R.id.textView_accelerometer_x)).setText(String.valueOf(event.values[0]));
-        ((TextView) findViewById(R.id.textView_accelerometer_y)).setText(String.valueOf(event.values[1]));
-        ((TextView) findViewById(R.id.textView_accelerometer_z)).setText(String.valueOf(event.values[2]));
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            ((TextView) findViewById(R.id.textView_accelerometer_x)).setText(String.valueOf(event.values[0]));
+            ((TextView) findViewById(R.id.textView_accelerometer_y)).setText(String.valueOf(event.values[1]));
+            ((TextView) findViewById(R.id.textView_accelerometer_z)).setText(String.valueOf(event.values[2]));
+        }
+
+        if(event.sensor.getType() == Sensor.TYPE_GRAVITY) {
+            ((TextView) findViewById(R.id.textView_gravity_x)).setText(String.valueOf(event.values[0]));
+            ((TextView) findViewById(R.id.textView_gravity_y)).setText(String.valueOf(event.values[1]));
+            ((TextView) findViewById(R.id.textView_gravity_z)).setText(String.valueOf(event.values[2]));
+        }
+
+        if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            ((TextView) findViewById(R.id.textView_gyroscope_x)).setText(String.valueOf(event.values[0]));
+            ((TextView) findViewById(R.id.textView_gyroscope_y)).setText(String.valueOf(event.values[1]));
+            ((TextView) findViewById(R.id.textView_gyroscope_z)).setText(String.valueOf(event.values[2]));
+        }
+
+
+/*
+        String messageStr="Hello Android!";
+        int server_port = 5555;
+        DatagramSocket s;
+        InetAddress local;
+        try {
+            s = new DatagramSocket();
+        }catch(java.net.SocketException e){
+
+        }
+
+        try {
+            local = InetAddress.getByName("192.168.0.2");
+        }catch (java.net.UnknownHostException e){
+
+        }
+        int msg_length=messageStr.length();
+        byte[] message = messageStr.getBytes();
+        DatagramPacket p = new DatagramPacket(message, msg_length,local,server_port);
+        s.send(p);
+*/
+
 
     }
 
